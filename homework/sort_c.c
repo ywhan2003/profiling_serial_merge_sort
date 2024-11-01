a5e866342039f3a43c11e93a13cd69c5499a02bc
ywhan2003 <2110520134@qq.com>
2024-11-01 15:23:20 +0800

    coarsening

--- homework/sort_c.c
+++ homework/sort_c.c
@@ -23,7 +23,98 @@
 
 #include "./util.h"
 
+static const int BASE_NUMBER = 10;
+
+// Function prototypes
+static void merge_c(data_t* A, int p, int q, int r);
+static void copy_c(data_t* source, data_t* dest, int n);
+static void base_c(data_t* A, int p, int r);
+
+// A basic merge sort routine that sorts the subarray A[p..r]
 void sort_c(data_t* A, int p, int r) {
-  printf("Unimplemented!\n");
+  assert(A);
+  if (p >= r) {
+    return;
+  }
+  
+  int num_element = r - p + 1;
+  if (num_element < BASE_NUMBER) {
+    base_c(A, p, r);
+    return;
+  }
+
+  int q = (p + r) / 2;
+  sort_c(A, p, q);
+  sort_c(A, q + 1, r);
+  merge_c(A, p, q, r);
 }
 
+// A merge routine. Merges the sub-arrays A [p..q] and A [q + 1..r].
+// Uses two arrays 'left' and 'right' in the merge operation.
+static void merge_c(data_t* A, int p, int q, int r) {
+  assert(A);
+  assert(p <= q);
+  assert((q + 1) <= r);
+  int n1 = q - p + 1;
+  int n2 = r - q;
+
+  data_t* left = 0, * right = 0;
+  mem_alloc(&left, n1 + 1);
+  mem_alloc(&right, n2 + 1);
+  if (left == NULL || right == NULL) {
+    mem_free(&left);
+    mem_free(&right);
+    return;
+  }
+
+  copy_c(A + p, left, n1);
+  copy_c(A + q + 1, right, n2);
+  *(left + n1) = UINT_MAX;
+  *(right + n2) = UINT_MAX;
+
+  int i = 0;
+  int j = 0;
+
+  for (int k = p; k <= r; k++) {
+    if (*(left + i) <= *(right + j)) {
+      *(A + k) = *(left + i);
+      i++;
+    } else {
+      *(A + k) = *(right + j);
+      j++;
+    }
+  }
+  mem_free(&left);
+  mem_free(&right);
+}
+
+static void copy_c(data_t* source, data_t* dest, int n) {
+  assert(dest);
+  assert(source);
+
+  for (int i = 0 ; i < n ; i++) {
+    *(dest + i) = *(source + i);
+  }
+}
+
+static void base_c(data_t* A, int p, int r) {
+  assert(A);
+  assert(p < r);
+
+  data_t* cur = A + p + 1;
+  data_t* begin = A + p;
+  data_t* end = A + r;
+
+  while (cur <= end) {
+    data_t val = *cur;
+    data_t* index = cur - 1;
+
+    while (index >= begin && *index > val) {
+      *(index + 1) = *index;
+      index--;
+    }
+
+    *(index + 1) = val;
+    cur++;
+  }
+}
