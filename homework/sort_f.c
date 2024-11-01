10e951fcab8df5cbe8e8abdb5449f01f3a8d77a8
ywhan2003 <2110520134@qq.com>
2024-11-01 16:45:17 +0800

    reuse temporary memory

--- homework/sort_f.c
+++ homework/sort_f.c
@@ -23,7 +23,105 @@
 
 #include "./util.h"
 
+static const int BASE_NUMBER = 10;
+data_t* temporary = 0;
+
+// Function prototypes
+static void merge_f(data_t* A, int p, int q, int r);
+static void copy_f(data_t* source, data_t* dest, int n);
+static void base_f(data_t* A, int p, int r);
+static void recursive_f(data_t* A, int p, int r);
+
+// A basic merge sort routine that sorts the subarray A[p..r]
 void sort_f(data_t* A, int p, int r) {
-  printf("Unimplemented!\n");
+  mem_alloc(&temporary, r - p + 1);
+  recursive_f(A, p, r);
+  mem_free(&temporary);
 }
 
+// A merge routine. Merges the sub-arrays A [p..q] and A [q + 1..r].
+// Uses two arrays 'left' and 'right' in the merge operation.
+static void merge_f(data_t* A, int p, int q, int r) {
+  assert(A);
+  assert(p <= q);
+  assert((q + 1) <= r);
+  int n1 = q - p + 1;
+  int n2 = r - q;
+
+  data_t* left = temporary, * right = A + q + 1;
+
+  copy_f(A + p, left, n1);
+
+  int i = 0; // Current index of the left part
+  int j = 0; // Current index of the right part
+  int k = p; // Current index of the previous array
+
+  for (; k <= r && i < n1 && j < n2; k++) {
+    if (*(left + i) <= *(right + j)) {
+      *(A + k) = *(left + i);
+      i++;
+    } else {
+      *(A + k) = *(right + j);
+      j++;
+    }
+  }
+
+  if (k > r) {
+    return;
+  }
+
+  if (i >= n1) {
+    copy_f(right + j, A + k, n2 - j);
+  }
+
+  copy_f(left + i, A + k, n1 - i);
+}
+
+static void copy_f(data_t* source, data_t* dest, int n) {
+  assert(dest);
+  assert(source);
+
+  for (int i = 0 ; i < n ; i++) {
+    *(dest + i) = *(source + i);
+  }
+}
+
+static void base_f(data_t* A, int p, int r) {
+  assert(A);
+  assert(p < r);
+
+  data_t* cur = A + p + 1;
+  data_t* begin = A + p;
+  data_t* end = A + r;
+
+  while (cur <= end) {
+    data_t val = *cur;
+    data_t* index = cur - 1;
+
+    while (index >= begin && *index > val) {
+      *(index + 1) = *index;
+      index--;
+    }
+
+    *(index + 1) = val;
+    cur++;
+  }
+}
+
+void recursive_f(data_t* A, int p, int r) {
+  assert(A);
+  if (p >= r) {
+    return;
+  }
+  
+  int num_element = r - p + 1;
+  if (num_element < BASE_NUMBER) {
+    base_f(A, p, r);
+    return;
+  }
+
+  int q = (p + r) / 2;
+  recursive_f(A, p, q);
+  recursive_f(A, q + 1, r);
+  merge_f(A, p, q, r);
+}
